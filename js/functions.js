bcfffde85f1d196377d1d9f52c19417eea78697e
brazilets <brazilets161@gmail.com>
2024-02-19 21:53:01 +0300

    Добавляет функцию для проверки длины строки

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+const checkStringLength = (checkString, maxLength) => checkString.length <= maxLength;
+
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
