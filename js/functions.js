6ebb216985252779c52b406b929b2f3d926da962
brazilets <brazilets161@gmail.com>
2024-02-20 20:56:18 +0300

    Добавляет функцию для проверки палиндрома

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,21 @@ const checkStringLength = (checkString, maxLength) => checkString.length <= maxL
 console.log(checkStringLength('проверяемая строка', 20));
 console.log(checkStringLength('проверяемая строка', 18));
 console.log(checkStringLength('проверяемая строка', 10));
+
+
+const checkPhrase = (palindromeString) => {
+  palindromeString = palindromeString.replaceAll(' ' , '').toLowerCase();
+
+  let checkPalindrome = '';
+
+  for (let i = palindromeString.length - 1; i >= 0; i--) {
+    checkPalindrome += palindromeString[i];
+  }
+
+  return palindromeString === checkPalindrome;
+};
+
+console.log(checkPhrase('топот'));
+console.log(checkPhrase('ДовОд'));
+console.log(checkPhrase('Кекс'));
+console.log(checkPhrase('Лёша на полке клопа нашёл'));
