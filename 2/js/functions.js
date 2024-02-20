7ce6da490d384d8fc4531c83e6b5f816c0fe75e9
Keks <keks@htmlacademy.ru>
2024-02-20 21:29:40 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,48 @@
+const checkStringLength = (checkString, maxLength) => checkString.length <= maxLength;
+
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
+
+
+const checkPhrase = (palindromeString) => {
+  palindromeString = palindromeString.replaceAll(' ' , '').toLowerCase();
+
+  let checkPalindrome = '';
+
+  for (let i = palindromeString.length - 1; i >= 0; i--) {
+    checkPalindrome += palindromeString[i];
+  }
+
+  return palindromeString === checkPalindrome;
+};
+
+console.log(checkPhrase('топот'));
+console.log(checkPhrase('ДовОд'));
+console.log(checkPhrase('Кекс'));
+console.log(checkPhrase('Лёша на полке клопа нашёл'));
+
+
+const sortsString = (checkSortsString) => {
+  let result = '';
+
+  checkSortsString = checkSortsString.toString();
+
+  for (let i = 0; i <= checkSortsString.length - 1; i++) {
+    if (Number.isNaN(parseInt(checkSortsString[i], 10)) === false) {
+      result += checkSortsString[i];
+    }
+  }
+
+  return result === '' ? NaN : Number(result);
+};
+
+console.log(sortsString('2023 год'));
+console.log(sortsString('1 кефир, 0.5 батона'));
+console.log(sortsString('ECMAScript 2022'));
+console.log(sortsString('агент 007'));
+console.log(sortsString('а я томат'));
+
+console.log(sortsString(2023));
+console.log(sortsString(-1));
+console.log(sortsString(1.5));
